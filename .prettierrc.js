f0fe598be3124563ce71f1dffa1fe7b02601fa73
Wind Blade <vn.chemgio@yahoo.com>
2022-08-14 15:58:31 +0700

    Init project with little dependency

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: false,
+    singleQuote: true,
+    trailingComma: 'all',
+
+    printWidth: 160,
+    tabWidth: 4,
+    semi: true,
+    endOfLine: 'lf',
+};
