ea5d5904a23a074869c84a8fcab926924b5d750c
Wind Blade <vn.chemgio@yahoo.com>
2022-09-06 12:04:37 +0700

    Bottom tab nav

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as ButtonAuthenticate }  from "./ButtonAuthenticate";
 export { default as IOSSegmentedControl } from "./IOSSegmentedControl";
 export { default as MaterialTextField }   from "./MaterialTextField";
+export { default as TabBarNavigationItem }   from "./TabBarNavigationItem";
+export { default as TabBarNavigationWeave }   from "./TabBarNavigationWeave";
